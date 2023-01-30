82f3740d55582315dc938043f1e660e992d20d2c
wingaibd <123862076+wingaibd@users.noreply.github.com>
2023-01-30 06:59:38 +0600

    Update script.js

--- client/script.js
+++ client/script.js
@@ -92,7 +92,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-0riz.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
